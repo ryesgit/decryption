12536cb812386de0e21df4cf7b7595735d56a420
User Rye <128949576+ryesgit@users.noreply.github.com>
2023-04-01 22:10:42 +0800

    Add row for displaying decrypted text

--- DecryptorUI.py
+++ DecryptorUI.py
@@ -12,6 +12,17 @@ sg.theme('DarkAmber')
 
 layout = [
     [sg.Text('THE DECRYPTOR')],
-    [sg.Text('Enter text to decrypt: '), sg.InputText()],
+    [sg.Text('Enter text to decrypt: '), sg.InputText(key='input')],
+    [sg.Text('Decrypted Text: '), sg.Text(key='output')],
     [sg.Button('Decrypt'), sg.Button('Quit')]
 ]
+
+window = sg.Window('Decryptor', layout)
+
+while True:
+    event, values = window.read()
+
+    if (event == sg.WIN_CLOSED or event == 'Quit'):
+        break
+
+window.close()

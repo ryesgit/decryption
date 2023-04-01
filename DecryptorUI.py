63531dde105b7f281ba5eca659b5ba321cc15323
User Rye <128949576+ryesgit@users.noreply.github.com>
2023-04-01 22:50:08 +0800

    Center title, and enable enter to evaluate text

--- DecryptorUI.py
+++ DecryptorUI.py
@@ -13,10 +13,10 @@ from decrypt import decrypt
 sg.theme('DarkAmber')
 
 layout = [
-    [sg.Text('THE DECRYPTOR')],
+    [sg.Text('', size=(20,1)), sg.Text('THE DECRYPTOR', font=('Any 20'), auto_size_text=True, justification='center'), sg.Text('', size=(20,1))],
     [sg.Text('Enter text to decrypt: '), sg.InputText(key='input')],
-    [sg.Text('Decrypted Text: '), sg.Text(key='output')],
-    [sg.Button('Decrypt'), sg.Button('Quit')]
+    [sg.Text('Decrypted Text: '), sg.Text(key='output', font=('Roboto', 14), background_color='black')],
+    [sg.Button('Decrypt', bind_return_key=True), sg.Button('Quit')]
 ]
 
 window = sg.Window('Decryptor', layout)
@@ -26,8 +26,12 @@ while True:
 
     if (event == sg.WIN_CLOSED or event == 'Quit'):
         break
+    
+    elif (event == 'Decrypt' or event == '\r'):
+        decrypted_text = decrypt(values['input'])
+        window['input'].update('')
+        window['output'].update(decrypted_text)
 
-    decrypted_text = decrypt(values['input'])
-    window['output'].update(decrypted_text)
+
 
 window.close()
